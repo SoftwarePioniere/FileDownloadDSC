1484be301bbf67d35a6ebf5de395138aca088790
Tobias Böker <tb@softwarepioniere.de>
2018-03-14 14:14:48 +0100

    publish with path

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,4 +1,6 @@
 #script to deploy with appveyor
-$env:PSModulePath = $env:PSModulePath + ";$(Get-Location)"
+#$env:PSModulePath = $env:PSModulePath + ";$(Get-Location)"
+
 Update-ModuleManifest -Path .\FileDownloadDSC\FileDownloadDSC.psd1 -ModuleVersion $env:APPVEYOR_BUILD_VERSION
-Publish-Module -Name FileDownloadDSC -NuGetApiKey $env:PS_GALLERY_API_KEY 
+Publish-Module -Path .\FileDownloadDSC -NuGetApiKey $env:PS_GALLERY_API_KEY 
+#Publish-Module -Name FileDownloadDSC -NuGetApiKey $env:PS_GALLERY_API_KEY 
